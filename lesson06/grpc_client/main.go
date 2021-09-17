078f765611bff169f2da1744bed3f56df4068327
weirubo <weirubo@gmail.com>
2021-09-17 22:40:28 +0800

    gRPC

---
+++ lesson06/grpc_client/main.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	pb "advanced_go/lesson06/proto"
+	"context"
+	"google.golang.org/grpc"
+	"log"
+	"os"
+	"time"
+)
+
+const (
+	address     = ":50051"
+	defaultName = "word"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure(), grpc.WithBlock())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewGreeterClient(conn)
+
+	name := defaultName
+	if len(os.Args) > 1 {
+		name = os.Args[1]
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second)
+	defer cancel()
+	r, err := c.SayHello(ctx, &pb.HelloRequest{Name: name})
+	if err != nil {
+		log.Fatalf("could not greet: %v", err)
+	}
+	log.Printf("Greeting: %s", r.GetMessage())
+}
